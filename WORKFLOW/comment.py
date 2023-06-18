44418ea516e3a3c5e47995e2f9f66a430195d542
Quest-Lord <135482571+Quest-Lord@users.noreply.github.com>
2023-06-18 15:25:12 -0700

    Update comment.py

--- WORKFLOW/comment.py
+++ WORKFLOW/comment.py
@@ -26,23 +26,23 @@ def upload_or_replace_file(token, repo_name, local_file_path, github_file_path, 
   except:
     repo.create_file(github_file_path, commit_message, content)
 
-download_file_from_github(os.getenv('TOKEN'), "Quest-Lord/Quest-Lord", "DATA/comments.list", "comments.list")
-
-with open("comments.list", "r") as comments:
-  comment = eval(comments.read())
-
-if len(comment) > 0:
-  comment_post = comment.pop(0)
-  with open("comments.list", "w") as comments:
-    comments.write(str(comment))
-
-  upload_or_replace_file(os.getenv('TOKEN'), "Quest-Lord/Quest-Lord", "comments.list", "DATA/comments.list", "Posted Comment")
-  print("here")
-
-  session = scratch3.login(os.getenv('QUESTLORD_USERNAME'), os.getenv('QUESTLORD_PASSWORD'))
-  print("here")
-
-  if comment_post[0][0] == "profile":
-    user = session.connect_user(str(comment_post[0][1]))
-    print("here")
-    print(user.post_comment(str(comment_post[0][2]), parent_id="", commentee_id=""))
+try:
+  download_file_from_github(os.getenv('TOKEN'), "Quest-Lord/Quest-Lord", "DATA/comments.list", "comments.list")
+  
+  with open("comments.list", "r") as comments:
+    comment = eval(comments.read())
+  
+  if len(comment) > 0:
+    comment_post = comment.pop(0)
+    with open("comments.list", "w") as comments:
+      comments.write(str(comment))
+  
+    upload_or_replace_file(os.getenv('TOKEN'), "Quest-Lord/Quest-Lord", "comments.list", "DATA/comments.list", "Posted Comment")
+  
+    session = scratch3.login(os.getenv('QUESTLORD_USERNAME'), os.getenv('QUESTLORD_PASSWORD'))
+  
+    if comment_post[0] == "profile":
+      user = session.connect_user(str(comment_post[1]))
+      print(user.post_comment(str(comment_post[2]), parent_id="", commentee_id=""))
+except Exception as e:
+  print(e)
