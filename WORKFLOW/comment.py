9cfb28f2354b4e1a9efe250b3bbe5937e33d5baa
Quest-Lord <135482571+Quest-Lord@users.noreply.github.com>
2023-06-18 13:57:30 -0700

    Update comment.py

--- WORKFLOW/comment.py
+++ WORKFLOW/comment.py
@@ -43,6 +43,6 @@ try:
 
     if comment_post[0][0] == "profile":
       user = session.connect_user(str(comment_post[0][1]))
-      user.post_comment(str(comment_post[0][2]), parent_id="", commentee_id="")
+      print(user.post_comment(str(comment_post[0][2]), parent_id="", commentee_id=""))
 except Exception as e:
   print(e)
